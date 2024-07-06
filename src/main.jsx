3227650bc16ba2ed475555e4e27af38af7df92d0
unknown <enockm1120@gmail.com>
2024-07-06 00:50:16 -0700

    src

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import {store} from './store'
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <Provider store={store}>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </Provider>,
